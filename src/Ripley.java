edae9f851b3a4aec211605213a39957694aebd8e
derlyhubelq-ops <derlyhubelq@gmail.com>
2026-05-15 12:06:20 -0500

    Update Ripley.java
    1.2

--- src/Ripley.java
+++ src/Ripley.java
@@ -2,5 +2,11 @@ import java.util.Scanner;
 public class Ripley {
   public static void main(String []args){
     Scanner escaner = new Scanner(System.in);
+    System.out.println("----RIPLAY PERÚ ");
+    System.out.println("Ingrese su nombre:")
+      String nombre = escaner.nextLine();
+  }
+  public static void bienvenida(String nombre){
+    System.out.println("Bienvenidos a ripley: "+ nombre);
   }
 }
