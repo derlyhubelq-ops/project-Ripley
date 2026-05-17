22c366de7177170b7842b1c5db8ec23c916174ed
derlyhubelq-ops <derlyhubelq@gmail.com>
2026-05-16 20:11:49 -0500

    Rename class Footloose

--- src/Ripley.java
+++ src/Ripley.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Ripley {
+public class Footloose {
   public static void main(String []args){
     Scanner escaner = new Scanner(System.in);
     System.out.println("----RIPLAY PERÚ ");
