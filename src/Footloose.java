4024d9dadd2e2724383b944a04abfc1ea39392d4
derlyhubelq-ops <derlyhubelq@gmail.com>
2026-05-18 10:08:28 -0500

    Refactor Footloose class and add menu options
    1.9

--- src/Footloose.java
+++ src/Footloose.java
@@ -1,13 +1,20 @@
-import java.util.Scanner;
+  import java.util.Scanner;
+  
 public class Footloose {
-  public static void main(String []args){
-    Scanner escaner = new Scanner(System.in);
-    System.out.println("----RIPLAY PERÚ ");
-    System.out.println("Ingrese su nombre:")
-      String nombre = escaner.nextLine();
-  }
-  public static void bienvenida(String nombre){
-    System.out.println("Bienvenidos a ripley: "+ nombre);
-    System.out.println("hola mundo");
-  }
+      public static void main(String[] args) {
+          Scanner escaner = new Scanner(System.in);
+          System.out.println("--- Sistema Footloose Inicializado ---");
+      }
+  
+      public static void bienvenida(String nombre) {
+         System.out.println("Bienvenido a Footloose: " + nombre);
+     }
+
+     public static void menuInicio() {
+         System.out.println("------ FOOTLOOSE ------");
+         System.out.println("1. Iniciar sesion");
+         System.out.println("2. Registrarse");
+         System.out.println("3. Entrar como invitado");
+        System.out.println("4. Salir");
+     }
 }
